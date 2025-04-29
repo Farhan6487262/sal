6c495ad6a3c658eb500aebd70c317c66aed0303c
Farhan6487262 <tajtechnovision8@gmail.com>
2025-04-29 12:00:23 +0500

    Update explore_page.py

--- explore_page.py
+++ explore_page.py
@@ -62,7 +62,7 @@ def show_explore_page():
     
     fig1, ax1 = plt.subplots()
     #ax1.pie(data,labels= data.index,autopct = "%1.1f%", shadow = True,startangle=90)
-    ax1.pie(data, labels=data.index, autopct="%1.1f%", shadow=True, startangle=90)
+    ax1.pie(data, labels=data.index, autopct="%1.1f%%", shadow=True, startangle=90)
 
     ax1.axis("equal")
     st.write("""£££ number of data from diffrent countries""")
