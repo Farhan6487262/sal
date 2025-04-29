883fb38f34869d03f909642613bfb7da5be7624a
Farhan6487262 <tajtechnovision8@gmail.com>
2025-04-29 12:09:22 +0500

    Update explore_page.py

--- explore_page.py
+++ explore_page.py
@@ -43,14 +43,7 @@ def load_data():
     df = df[df["Employment"] == 0]
     
     df = df.drop("Employment", axis=1)
-    country_map = shorten_categories(df.Country.value_counts(), 400)
-    df['Country'] = df['Country'].map(country_map)
-    df = df[df["ConvertedComp"] <= 250000]
-    df = df[df["ConvertedComp"] >= 10000]
-    df = df[df['Country'] != 151]
-    df["YearsCode"] = df["YearsCode"].apply(clean_experience)
-    df["EdLevel"] = df["EdLevel"].apply(clean_education)
-
+  
 
 
 
