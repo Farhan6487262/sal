716b2948a0225ecc2579bc377e2c028a15fd730e
Farhan6487262 <tajtechnovision8@gmail.com>
2025-04-29 13:06:57 +0500

    Update predict_page.py

--- predict_page.py
+++ predict_page.py
@@ -35,7 +35,8 @@ def show_predict_page():
 
 
     # User input fields
-    country =  st.text_input("Country", countries)
+    #country =  st.text_input("Country", countries)
+    country = st.text_input("Country", "")
     education_level = st.selectbox("Education level", education)
     experience = st.slider("Years of Experience", 0, 50, 3)
 
